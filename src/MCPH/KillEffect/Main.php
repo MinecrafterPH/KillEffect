b6abe17432d39e4ea259df95dab3765ae075afb6
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-05-28 14:35:06 +0800

    Create Main.php

---
+++ src/MCPH/KillEffect/Main.php
@@ -0,0 +1,81 @@
+<?php
+
+
+namespace MCPH\KillEffect\Main;
+
+
+use pocketmine\utils\TextFormat as MT;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\Player;
+use pocketmine\server;
+use pocketmine\level;
+use pocketmine\item\Item;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\math\Vector3;
+use onebone\economyapi\EconomyAPI;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+
+	class KillEffect extends PluginBase implements Listener
+	
+	{
+		public function onEnable()
+		{
+		        $this->saveDefaultConfig();
+		        $this->reloadConfig();	
+			$this->getServer()->getPluginManager()->registerEvents($this,$this);
+			$this->getLogger()->info("KillEffect has been enabled.");
+			$this->api = EconomyAPI::getInstance();
+		}
+	
+		public function onPlayerDeathEvent(PlayerDeathEvent $event)
+		{
+			$cfg = $this->getConfig();
+			$effectid = $cfg->get("Effect-ID");
+			$duration = $cfg->get("Duration");
+			$particles = $cfg->get("Particles");
+			$amplifier = $cfg->get("Amplifier");
+			
+			$add = $cfg->get("Add-Money")
+			$reduce = $cfg->get("Reduce-Money")
+			
+			$effect = Effect::getEffect($effectid); //Effect ID
+	                $effect->setVisible($particles); //Particles
+	                $effect->setAmplifier($amplifier);
+	                $effect->setDuration($duration); //Ticks
+			
+			$player = $event->getEntity();
+			$name = strtolower($player->getName());
+		
+			if ($player instanceof Player)
+			{
+				$cause = $player->getLastDamageCause();
+		
+				if($cause instanceof EntityDamageByEntityEvent)
+				{
+					$damager = $cause->getDamager();
+					
+					if($damager instanceof Player)
+					{
+						$damager->sendMessage("You killed $player.\nYou earn $$add for getting a kill and a health boost for $duration seconds.");
+						$damager->addEffect($effect);
+						$this->api->addMoney($damager, $add);
+						
+						$player->sendMessage("You were killed by $damager.\nYou lose $$reduce for getting killed.");
+						$this->api->reduceMoney($player, $reduce);
+					}
+				}
+			}
+		}
+
+		
+		public function onDisable()
+		{
+			$this->getLogger()->info("Plugin unloaded!");
+		}
+	}
